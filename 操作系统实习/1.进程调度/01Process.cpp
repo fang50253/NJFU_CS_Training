62325fbf025016187fea07cc7145f88b21fab765
fang50253 <fzy1139950253@gmail.com>
2024-12-20 14:52:16 +0800

    更新操作系统实习

---
+++ 操作系统实习/1.进程调度/01Process.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<iostream>
+#include<stdexcept>
+#incldue<string.h>
+struct Process
+{
+    char process_name;//进程名称
+    int time_arrive;//到达时间
+    int time_serve;//服务时间
+};
+class RR
+{
+    private:
+
+    public:
+
+};
+class SJF
+{
+    private:
+    
+    public:
+
+};
+int main()
+{
+
+}
