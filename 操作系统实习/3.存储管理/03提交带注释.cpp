d87b29dfe2c5d72c2ed9d34d0383271ce1148db9
fang50253 <fzy1139950253@gmail.com>
2024-12-21 13:06:11 +0800

    撰写完1 3的实习报告

--- 操作系统实习/3.存储管理/03提交带注释.cpp
+++ 操作系统实习/3.存储管理/03提交带注释.cpp
@@ -7,26 +7,26 @@
 namespace fzy//创建命名空间fzy，用于写一些数据结构
 {
     template<typename T>
-    struct queuenode 
+    struct queuenode //创建队列结点
     {
-        T v = T();
-        queuenode<T>* next = nullptr;
+        T v = T();//提供队列结点的默认构造方法
+        queuenode<T>* next = nullptr;//指向下一个位置，并提供默认构造方法
     };
-    template <typename T>
+    template <typename T>//创建队列模板类
     class queue 
     {
     private:
         queuenode<T>* head;
         queuenode<T>* last;
-        int size_;
+        int size_;//队列的大小
     public:
-        queue() 
+        queue() //队列模板的默认构造方法
         {
             head = new queuenode<T>;
             last = head;
             size_ = 0;
         }
-        queue(const queue<T>& other) 
+        queue(const queue<T>& other) //提供带参的构造函数
         {
             head = new queuenode<T>;
             last = head;
@@ -38,7 +38,7 @@ namespace fzy//创建命名空间fzy，用于写一些数据结构
                 current = current->next;
             }
         }
-        queue<T>& operator=(const queue<T>& other)//重载队列拷贝函数
+        queue<T>& operator=(const queue<T>& other)//重载队列拷贝函数，深拷贝
         {
             if (this != &other) 
             {
@@ -52,24 +52,24 @@ namespace fzy//创建命名空间fzy，用于写一些数据结构
             }
             return *this;
         }
-        ~queue() 
+        ~queue() //析构函数
         {
             clear();
             delete head;
         }
-        void clear() 
+        void clear() //清空队列
         {
             while (!empty()) pop();
         }
-        void push(T v) 
+        void push(T v) //将元素压入队列
         {
-            queuenode<T>* newnode = new queuenode<T>;
+            queuenode<T>* newnode = new queuenode<T>;//创建一个新的结点
             newnode->v = v;
             last->next = newnode;
             last = newnode;
             ++size_;
         }
-        void pop() 
+        void pop() //弹出
         {
             if (!empty()) 
             {
@@ -79,26 +79,28 @@ namespace fzy//创建命名空间fzy，用于写一些数据结构
                 delete temp;
                 --size_;
             }
+            else 
+            throw std::runtime_error("queue_empty");
         }
-        T front() const 
+        T front() const //返回队列队首
         {
             if (!empty()) return head->next->v;
             throw std::runtime_error("Queue is empty");
         }
-        T back() const 
+        T back() const //返回队列队尾
         {
             if (!empty()) return last->v;
             throw std::runtime_error("Queue is empty");
         }
-        bool empty() const 
+        bool empty() const //队列判空
         {
             return size_ == 0;
         }
-        int size() const 
+        int size() const //返回队列的长度
         {
             return size_;
         }
-        void print() const 
+        void print() const //输出队列，用于调试
         {
             queuenode<T>* current = head->next;
             while (current != nullptr) 
@@ -118,7 +120,7 @@ struct p_str
     int clock_revise;// 改进 Clock 的修改标记
     int LRU_time;    // LRU 的未使用时间
     p_str(int pagenum = -1, int count = -1, int clock_visit = -1, int clock_revise = -1, int LRU_time = -1)
-        : pagenum(pagenum), count(count), clock_visit(clock_visit), clock_revise(clock_revise), LRU_time(LRU_time) {}
+        : pagenum(pagenum), count(count), clock_visit(clock_visit), clock_revise(clock_revise), LRU_time(LRU_time) {}//提供构造方法
 };
 class storage 
 {
@@ -131,29 +133,30 @@ private:
     void init() 
     {
         effective_times = 0;
-        while (!fifo.empty()) fifo.pop();
-        while (!clock.empty()) clock.pop();
-        while (!lru_queue.empty()) lru_queue.pop();
+        while (!fifo.empty()) fifo.pop();//初始化每一个数据结构
+        while (!clock.empty()) clock.pop();//初始化每一个数据结构
+        while (!lru_queue.empty()) lru_queue.pop();//初始化每一个数据结构
     }
     void print(const char way[],int Page_num,int RAM,double success)
     {
         printf("This is %s:\nPage_num=%d,RAM=%dPages,success_rate=%.1lf %%\n\n\n",way,Page_num,RAM,success/4);
+        //输出结果
     }
 public:
     storage(int n) : n(n) {}//创建默认构造函数
     void FIFO() 
     {
-        init();
+        init();//初始化
         for (int i = 0; i < MAXSIZE; ++i) {
             int t = 0;
             int page=rand()%400/10;// 检查 FIFO 队列是否命中
             fzy::queue<p_str>temp=fifo;
             while (!temp.empty()) 
             {
-                if (temp.front().pagenum == page) 
+                if (temp.front().pagenum == page) //在页表中
                 {
                     ++t;
-                    ++effective_times;
+                    ++effective_times;//有效
                     break;
                 }
                 temp.pop();
@@ -161,7 +164,7 @@ public:
             // 未命中则进行置换
             if (!t) 
             {
-                if ((int)fifo.size() < n) fifo.push(p_str(page, 1));
+                if ((int)fifo.size() < n) fifo.push(p_str(page, 1));//队列没有满
                 else 
                 {
                     fifo.pop();            // 移除队首页面
@@ -169,7 +172,7 @@ public:
                 }
             }
         }
-        print("FIFO",MAXSIZE,n,effective_times);
+        print("FIFO",MAXSIZE,n,effective_times);//输出
     }
     void LRU() 
     {
@@ -198,8 +201,9 @@ public:
             // 未命中则置换
             if (!t) 
             {
-                if ((int)lru_queue.size() < n) lru_queue.push(p_str(page, 1, -1, -1, 0));
-                else 
+                if ((int)lru_queue.size() < n) //队列没有满
+                lru_queue.push(p_str(page, 1, -1, -1, 0));
+                else //队列满了，找到最久没有使用的页面
                 {
                     // 找到最久未使用页面
                     fzy::queue<p_str> temp = lru_queue;
@@ -228,7 +232,7 @@ public:
                 }
             }
         }
-        print("LRU",MAXSIZE,n,effective_times);
+        print("LRU",MAXSIZE,n,effective_times);//输出结果
     }
     void Improved_Clock() 
     {
@@ -237,9 +241,9 @@ public:
         {
             int t = 0;
             int page = rand() % 400 / 10;
-            int m = rand() % 2;
+            int m = rand()>>1;
             // 检查 Clock 队列是否命中
-            fzy::queue<p_str> temp = clock;
+            fzy::queue<p_str>temp=clock;
             while (!temp.empty()) 
             {
                 if (temp.front().pagenum == page) 
@@ -255,10 +259,10 @@ public:
                 temp.pop();
             }
             // 未命中则置换
-            if (!t) 
+            if(!t)
             {
-                if ((int)clock.size() < n) clock.push(p_str(page, 1, 1, m));
-                else 
+                if ((int)clock.size() < n) clock.push(p_str(page, 1, 1, m));//队列没有满
+                else //队列满了，则只能置换
                 {
                     // 查找替换目标
                     while (true) 
@@ -279,7 +283,7 @@ public:
                 }
             }
         }
-        print("Improved_Clock",MAXSIZE,n,effective_times);
+        print("Improved_Clock",MAXSIZE,n,effective_times);//输出
     }
 };
 int main() 
@@ -287,11 +291,10 @@ int main()
     int n;
     srand((unsigned)time(NULL));
     scanf("%d", &n);
-    printf("\n\n\n");
+    printf("\n");
     storage storage(n);
-    storage.FIFO();
-    storage.LRU();
-    storage.Improved_Clock();
-    printf("\n\n\n");
+    storage.FIFO();//先进先出
+    storage.LRU();//最近最久未使用
+    storage.Improved_Clock();//改进的时钟
     return 0;
 }
